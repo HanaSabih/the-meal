e708b275ad8249ed3fc39b2bc59a2026ce692756
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-02-07 12:53:04 +0300

    install file loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 
