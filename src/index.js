ed2ebc1513f85acb6f724e553626ca76010f09a0
hana <hanasabih@gmail.com>
2023-02-10 17:14:49 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { getData } from './modules/mealsMenu.js';
+import { getData, getOptionsData } from './modules/mealsMenu.js';
 
 import logoSrc from './images/logo.png';
 
@@ -12,3 +12,4 @@ logoLink.appendChild(logo);
 const menuListUrl = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
 getData(menuListUrl);
+getOptionsData('Beef');
