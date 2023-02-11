c3e07c5d185a65c34cc902c7ce63ab00154aa9d9
Alwyn A. S <alwynsimiyu@gmail.com>
2023-02-11 11:51:04 +0300

    Revert "Revert 35 final commit"

---
+++ src/__tests__/commentcounter.test.js
@@ -0,0 +1,17 @@
+/** @jest-environment jsdom */
+
+import getCount from '../modules/commentcounter.js';
+
+describe('count displayed comments for each food item', () => {
+  document.body.innerHTML = `
+  <ul class="comments">
+    <li class="shown-comment"></li>
+    <li class="shown-comment"></li>
+    <li class="shown-comment"></li>
+    <li class="shown-comment"></li>
+  </ul>`;
+
+  test('counted comments should be 4', async () => {
+    expect(getCount()).toEqual(4);
+  });
+});
